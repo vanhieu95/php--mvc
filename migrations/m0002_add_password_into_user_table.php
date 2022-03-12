897921ced9a913941a0845cc01e3f49b2946528b
vanhieu95 <nguyenluongtrunghieu1995@gmail.com>
2022-03-12 23:28:26 +0700

    implement migrations

---
+++ migrations/m0002_add_password_into_user_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use app\core\Application;
+
+class m0002_add_password_into_user_table
+{
+  public function up()
+  {
+    $database = Application::$app->database;
+    $sqlQuery = "ALTER TABLE user ADD COLUMN password VARCHAR(255) NOT NULL;";
+    $database->pdo->exec($sqlQuery);
+  }
+
+  public function down()
+  {
+    $database = Application::$app->database;
+    $sqlQuery = "ALTER TABLE user DROP COLUMN password VARCHAR(255);";
+    $database->pdo->exec($sqlQuery);
+  }
+}
